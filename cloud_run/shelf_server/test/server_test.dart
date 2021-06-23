9666f124fa751719315b75744db12c0f3fedf29f
Tony Pujals <tonypujals@google.com>
2021-06-22 21:00:38 -0700

    wip

--- cloud_run/shelf_server/test/server_test.dart
+++ cloud_run/shelf_server/test/server_test.dart
@@ -1,28 +1,30 @@
-import 'dart:io';
+import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:test/test.dart';
 import 'package:test_process/test_process.dart';
 
+// Test a running instance of the server.
 void main() {
-  // Port value of 0 will cause an available port to be chosen at random.
-  const initPort = '0';
   late int assignedPort;
   late String baseUrl;
   late TestProcess proc;
 
   group('test server', () {
     setUp(() async {
+      // Start server on a random port (by using '0' for port value).
       proc = await TestProcess.start(
         'dart',
         ['run', 'bin/server.dart'],
-        environment: {'PORT': initPort},
+        environment: {'PORT': '0'},
       );
-
       var output = await proc.stdout.next;
+
+      // Determine URL using actual port that was assigned for subsequent tests.
       var parts = output.split('Server listening on port ');
       assignedPort = int.parse(parts[1].trim());
       baseUrl = 'http://localhost:$assignedPort';
+
       print('- test server started on port $assignedPort');
     });
 
@@ -50,10 +52,27 @@ void main() {
       test('/time', () async {
         final response = await get(Uri.parse('$baseUrl/time'));
         expect(response.statusCode, 200);
+
         var time = DateTime.parse(response.body);
         var now = DateTime.now();
-        expect(time, predicate<DateTime>((t) => t.difference(now).inSeconds < 1,
-            'Server time ($time) should be within a second of current time ($now)'));
+        expect(
+            time,
+            predicate<DateTime>((t) => t.difference(now).inSeconds < 1,
+                'Server time ($time) should be within a second of current time ($now)'));
+        await proc.kill();
+      }, timeout: defTimeout);
+
+      test('/time?format=json', () async {
+        final response = await get(Uri.parse('$baseUrl/time?format=json'));
+        expect(response.statusCode, 200);
+
+        var body = JsonDecoder().convert(response.body);
+        var time = DateTime.parse(body['time']);
+        var now = DateTime.now();
+        expect(
+            time,
+            predicate<DateTime>((t) => t.difference(now).inSeconds < 1,
+                'Server time ($time) should be within a second of current time ($now)'));
         await proc.kill();
       }, timeout: defTimeout);
 
@@ -62,7 +81,6 @@ void main() {
         expect(response.statusCode, 404);
         await proc.kill();
       });
-
     });
   });
 }
